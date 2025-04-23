f874a069c2b1d565e7e4fb8bbaff06b86059abb4
Meryem Uludağlı <meryemuslu2035@gmail.com>
2025-04-23 14:50:47 +0300

    add

--- app.js
+++ app.js
@@ -9,32 +9,50 @@ const helmet = require("helmet");
 const sanitize = require("express-mongo-sanitize");
 const hpp = require("hpp");
 
+// express uygulamsı oluştur
 const app = express();
 
-rateLimit({
+const limiter = rateLimit({
   max: 100,
   windowMs: 15 * 60 * 1000,
   message:
-    "Kisa sure icinde cok fazla istekte bulundunuz.Daha sonra tekrar deneyin.",
+    "Kısa süre içerisinde çok fazla istekte bulundunuz. Lütfen daha sonra tekrar deneyin",
 });
 
+//! middleware
+
+// client'a gönderilen cevaba güvenlik amaçlı http headerları ekler
 app.use(helmet());
+
+// rate limit: aynı ip adresinden belirli bir süre içerisinde gelebilecek istek sınırını belirle
 app.use("/api", limiter);
+
+// client'tan gelen json versini js'e çevir (maks kota)
 app.use(express.json({ limit: "10kb" }));
+
+// (body/params/headers/query) alanlarında mongodb kodu tespit ettiği zaman boza
 app.use(sanitize());
 
+// hpp: parametre kirliliğini önler
 app.use(hpp());
+
+// client'tan gelen çerezleri işler
 app.use(cookieParser());
 
+// router'ları projeye tanıt
 app.use("/api/tours", tourRouter);
 app.use("/api/users", userRouter);
 app.use("/api/reviews", reviewRouter);
 
+// tanımlanmayan bir route'a istek atıldığında hata ver
 app.all("*", (req, res, next) => {
   const err = error(404, "İstek attığınız yol mevcut değil");
+
+  // yeni yöntem
   next(err);
 });
 
+// hata olduğunda devreye giren mw
 app.use((err, req, res, next) => {
   console.log(err.stack);
 
@@ -48,4 +66,5 @@ app.use((err, req, res, next) => {
   });
 });
 
+// server.js'de kullanmak için export ettik
 module.exports = app;
